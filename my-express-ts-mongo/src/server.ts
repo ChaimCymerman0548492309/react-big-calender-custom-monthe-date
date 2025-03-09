553c2b4517d0c7a2f042e2ae16fbb53a5d37a86d
chaim <a548492309@gmail.com>
2025-03-09 20:52:44 +0200

    add

--- my-express-ts-mongo/src/server.ts
+++ my-express-ts-mongo/src/server.ts
@@ -30,16 +30,16 @@ mongoose.connect(mongoURI, {
     // הדפס את ה-stack trace המלא
     console.error('Stack trace:', error.stack);
   });
-// בדיקת התחברות ל-MongoDB
-const db = mongoose.connection;
+// // בדיקת התחברות ל-MongoDB
+// const db = mongoose.connection;
 
-db.on('error', (error) => {
-  console.error('❌ Connection error to MongoDB:', error);
-});
+// db.on('error', (error) => {
+//   console.error('❌ Connection error to MongoDB:', error);
+// });
 
-db.once('open', () => {
-  console.log('✅ Successfully connected to MongoDB!');
-});
+// db.once('open', () => {
+//   console.log('✅ Successfully connected to MongoDB!');
+// });
 
 // הגדרת סכמה עבור User
 interface IUser extends Document {
